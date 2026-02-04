f260fa8239d121f3ecace0f4a992c5ee8ded9d35
FyreTeGo <abdullahalnasrallah123@gmail.com>
2026-02-04 21:38:13 +0300

    Add files via upload

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -26,29 +26,33 @@ public class SinglyLinkedList<E> {
     private Node<E> head = null;
     private Node<E> tail = null;
     private int size = 0;
-    public SinglyLinkedList() {}
+
+    public SinglyLinkedList() {
+    }
 
     public int getSize() {
         return size;
     }
+
     public boolean isEmpty() {
         if (size == 0)
             return true;
         else
             return false;
     }
+
     public void addFirst(E value) {
         Node<E> node = new Node<>(value, null);
-        if (isEmpty()){
+        if (isEmpty()) {
             head = node;
             tail = node;
-        }
-        else {
+        } else {
             node.next = head;
             head = node;
         }
         size++;
     }
+
     public void addLast(E value) {
         Node<E> node = new Node<>(value, null);
         if (isEmpty())
@@ -58,6 +62,7 @@ public class SinglyLinkedList<E> {
         tail = node;
         size++;
     }
+
     public E removeFirst() {
         if (isEmpty())
             return null;
@@ -72,6 +77,7 @@ public class SinglyLinkedList<E> {
         }
 
     }
+
     public void findNode(E value) {
         Node<E> current = head;
         int counter = 0;
@@ -79,7 +85,7 @@ public class SinglyLinkedList<E> {
             counter++;
             if (current.getElement().equals(value)) {
                 System.out.println("found the value \"" + current.getElement()
-                + "\" at node " + counter);
+                        + "\" at node " + counter);
                 return;
             }
             current = current.next;
@@ -87,17 +93,71 @@ public class SinglyLinkedList<E> {
         System.out.println("did not find node");
     }
 
-
-    static void main() {
-        SinglyLinkedList list = new SinglyLinkedList<>();
-
-        list.addLast("IAU");
-        list.addLast("PMU");
-        list.addLast("KFUPM");
-        list.addLast("KFU");
-        list.findNode("KFU");
+    public void addNode(E value, int index) {
+        if (index < 0 || index > getSize()) {
+            System.out.println("error, cannot add a node at this" +
+                    "position");
+        } else if (index == 0) {
+            addFirst(value);
+        } else if (index == getSize()) {
+            addLast(value);
+        } else {
+            Node<E> node = new Node<>(value, null);
+            Node<E> current = head;
+            for (int i = 0; i < (index - 1); i++) {
+                current = current.next;
+            }
+            node.next = current.next;
+            current.next = node;
+            size++;
+        }
     }
 
+    public void removeNode(E value) {
+        Node<E> current = head;
+        Node<E> previous = head;
+        while (!current.getElement().equals(value)) {
+            if (current.next == null) {
+                System.out.println("value does not exist");
+                return;
+            }
+            else {
+                previous = current;
+                current = current.next;
+            }
+        } // I got help from internet for while loop condition
+        if (current == head) {
+            removeFirst();
+        }
+        else {
+            previous.next = current.next;
+            size--;
+        }
+        if (current == tail) {
+            tail = previous;
+        }
+    }
+
+    public void display() {
+        Node<E> current;
+        current = head;
+        int counter = 0;
+        while (current != null) {
+            counter++;
+            System.out.println("Linked List ( " + counter + " ): " + current.getElement());
+            current = current.next;
+        }
+    }
+
+    static void main() {
+        Person p1 = new Person("Abdullah", "Dammam");
+        Person p2 = new Person("Mohammed", "Saihat");
+
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.addFirst(p1);
+        list.addLast(p2);
+        list.display();
+    }
 }
 
 
